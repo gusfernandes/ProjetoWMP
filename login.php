32ef91bd14e7b53ecd1b0df0699fc84a153ebfa7
gusfernandes <guga.spfc.guga@gmail.com>
2021-11-05 00:28:52 -0300

    Atualização Projeto WMP

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/login.css">
+    <title>Where's My Player</title>
+</head>
+<body>
+    <div class="Login">
+        <img src="./img/logo.png" alt="Logo do Site" height="70" width="70">
+    <h2>Faça seu login aqui</h1>
+    <form>
+        <input type="username" placeholder="Nome de usuário"><br>
+        <input type="password" placeholder="Escreva a sua senha"><br>
+        <input type="submit" value="Entrar">
+    </form>
+    </div>
+</body>
+</html>
---
+++ usuario.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 05-Nov-2021 às 04:27
+-- Versão do servidor: 10.4.17-MariaDB
+-- versão do PHP: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bd_wmp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `usu_name` varchar(50) NOT NULL,
+  `usu_mail` varchar(50) NOT NULL,
+  `usu_nick` varchar(20) NOT NULL,
+  `usu_pass` varchar(20) NOT NULL,
+  `usu_idade` int(11) NOT NULL,
+  `usu_date` date NOT NULL DEFAULT current_timestamp(),
+  `usu_foto` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
